082ab17dfe8f0307c0269bd2c921ffbac53ef193
PEzwartsEDU <pe.zwarts@student.alfa-college.nl>
2026-01-28 18:29:08 +0100

    Update

--- src/Game.cs
+++ src/Game.cs
@@ -18,23 +18,24 @@ class Game
 	{
 		// Level 0
 
-		Room entrance_dungeon = new Room("[CAMPUS] You are in front of a flight of stone stairs ?HMMMMM?");
+		Room dungeon_d = new Room("[CAMPUS] You are in front of a flight of stone stairs ?HMMMMM?");
 		Room dungeon = new Room("[DUNGGEON] You are in an dungeon !");
 
-		Room hallway0_1 = new Room("[DUNGEON] You are in an very long hallway !");
-		Room hallway0_2 = new Room("[DUNGEON] You are in an long hallway !");
-		Room door0_1 = new Room("You are in front of an locked door !");
-		Room hallway0_3 = new Room("[DUNGEON] You are in an hallway !");
+		Room hallway0 = new Room("[DUNGEON] You are in an hallway !");
+		Room door0 = new Room("You are in front of an locked door !");
 
 		Room den = new Room("[DUNGEON] You are in an dragon's lair !");
 
-		Room hallway1_1 = new Room("[DUNGEON] You are in an hallway !");
-		Room door1_1 = new Room("[DUNGEON] You are in an open door !");
+		Room hallway1 = new Room("[DUNGEON] You are in an hallway !");
+		Room door1 = new Room("[DUNGEON] You are in an open door !");
 
+		// Might not be used.
 		Room secret = new Room("[DUNGEON] You are in an secret room !");
+
 		Room portal = new Room("[DUNGEON] You are in front of an wormhole; Enter?");
-		Room white_house = new Room("[WHITE_HOUSE] You are in the White House of the U.S.A !");
-		Room oval_office = new Room("[OVAL_OFFICE] You are in front of sleepy sleepy donald ...");
+
+		//Room white_house = new Room("[WHITE_HOUSE] You are in the White House of the U.S.A !");
+		//Room oval_office = new Room("[OVAL_OFFICE] You are in front of sleepy sleepy donald ...");
 
 		// Level 1
 
@@ -46,36 +47,29 @@ class Game
 
 		// Level 0
 
-		Room valhalla = new Room("[VALHALLA] You gave reached valhalla. Game over.");
+		//Room valhalla = new Room("[VALHALLA] You gave reached valhalla. Game over.");
 
-		entrance_dungeon.AddExit("s", campus);
-		entrance_dungeon.AddExit("n", dungeon);
+		dungeon_d.AddExit("s", campus);
+		dungeon_d.AddExit("n", dungeon);
 
-		dungeon.AddExit("s", entrance_dungeon);
-		dungeon.AddExit("n", hallway0_1);
+		dungeon.AddExit("s", dungeon_d);
+		dungeon.AddExit("n", hallway0);
 		
-		hallway0_1.AddExit("n", hallway0_2);
-		hallway0_1.AddExit("s", dungeon);
+		hallway0.AddExit("n", den);
+		hallway0.AddExit("s", dungeon);
+		hallway0.AddExit("e", door0);
 
-		hallway0_1.AddExit("e", door0_1);
-		door0_1.AddExit("w", hallway0_1);
+		den.AddExit("s", hallway0);
+		den.AddExit("w", hallway1);
 
-		hallway0_2.AddExit("n", hallway0_3);
-		hallway0_2.AddExit("s", hallway0_2);
+		hallway1.AddExit("n", door1);
+		hallway1.AddExit("e", den);
 
-		hallway0_3.AddExit("n", den);
-		hallway0_3.AddExit("s", hallway0_3);
-
-		den.AddExit("w", hallway1_1);
-
-		hallway1_1.AddExit("n", door1_1);
-		hallway1_1.AddExit("e", den);
-
-		door1_1.AddExit("s", hallway1_1);
+		door1.AddExit("s", hallway1);
 
 		// Level 1
 		
-		campus.AddExit("n", entrance_dungeon);
+		campus.AddExit("n", dungeon_d);
 		campus.AddExit("s", lab);
 		campus.AddExit("e", theatre);
 		campus.AddExit("w", pub);
