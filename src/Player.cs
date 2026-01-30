511ff957ea4e746dd1d635807a59ebaf152c3cba
PEzwartsEDU <pe.zwarts@student.alfa-college.nl>
2026-01-30 12:23:35 +0100

    Update; player.cs

--- src/Player.cs
+++ src/Player.cs
@@ -1,8 +1,18 @@
 class Player {
     public Room CurrentRoom { get; set; }
 
+    private int health;
+
     public Player()
     {
         CurrentRoom = null;
+
+        health = 100;
     }
+    
+    public void Damage() {}
+
+    public void Heal() {}
+
+    public void IsAlive() {}
 }
