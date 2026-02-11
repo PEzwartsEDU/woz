a7eedae79f6ba571b730286e14115cefb9061427
PEzwartsEDU <PEzwarts>
2026-02-11 13:37:08 +0100

    Update; Remove comments

--- src/Game.cs
+++ src/Game.cs
@@ -2,12 +2,10 @@ using System;
 
 class Game
 {
-	// Private fields
 	private Parser parser;
 	private Player player;
 	//private Room currentRoom;
 
-	// Constructor
 	public Game()
 	{
 		parser = new Parser();
@@ -15,7 +13,6 @@ class Game
 		CreateRooms();
 	}
 
-	// Initialise the Rooms (and the Items)
 	private void CreateRooms()
 	{
 		// Level 0
@@ -85,24 +82,14 @@ class Game
 		lab.AddExit("e", office);
 
 		office.AddExit("w", lab);
-		
-		// Create your Items here
-		// ...
-		// And add them to the Rooms
-		// ...
-
-		// Start game outside
 
 		player.CurrentRoom = campus;
 	}
 
-	//  Main play routine. Loops until end of play.
 	public void Play()
 	{
 		PrintWelcome();
 
-		// Enter the main command loop. Here we repeatedly read commands and
-		// execute them until the player wants to quit.
 		bool finished = false;
 		while (!finished)
 		{
@@ -118,7 +105,6 @@ class Game
 		Console.ReadLine();
 	}
 
-	// Print out the opening message for the player.
 	private void PrintWelcome()
 	{
 		Console.WriteLine("WORLD OF ZUUL.\n");
@@ -129,9 +115,6 @@ class Game
 		Console.WriteLine(player.CurrentRoom.GetLongDescription());
 	}
 
-	// Given a command, process (that is: execute) the command.
-	// If this command ends the game, it returns true.
-	// Otherwise false is returned.
 	private bool ProcessCommand(Command command)
 	{
 		bool wantToQuit = false;
@@ -163,35 +146,24 @@ class Game
 
 		return wantToQuit;
 	}
-
-	// ######################################
-	// implementations of user commands:
-	// ######################################
 	
-	// Print out some help information.
-	// Here we print the mission and a list of the command words.
 	private void PrintHelp()
 	{
 		Console.WriteLine("You are lost. You are alone.");
 		Console.WriteLine("You wandering around.\n");
-		// let the parser print the commands
 		parser.PrintValidCommands();
 	}
 
-	// Try to go to one direction. If there is an exit, enter the new
-	// room, otherwise print an error message.
 	private void GoRoom(Command command)
 	{
 		if(!command.HasSecondWord())
 		{
-			// if there is no second word, we don't know where to go...
 			Console.WriteLine("Go where?");
 			return;
 		}
 
 		string direction = command.SecondWord;
 
-		// Try to go to the next room.
 		Room nextRoom = player.CurrentRoom.GetExit(direction);
 		if (nextRoom == null)
 		{
