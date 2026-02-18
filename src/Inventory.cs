43d99634c4850abfb289fcb8e0f2c67d795ef223
PEzwartsEDU <PEzwarts>
2026-02-18 09:51:13 +0100

    Update

--- src/Inventory.cs
+++ src/Inventory.cs
@@ -1,44 +1,26 @@
 class Inventory {
-    // fields
     private int MaxWeight;
     private Dictionary<string, Item> items;
     
-    // constructor
     public Inventory(int maxWeight) {
         this.maxWeight = maxWeight;
         this.items = new Dictionary<string, Item>();
     }
 
-    // methods
     public bool Put(string itemName, Item item) {
-        // TODO implementeer:
-        // Check het gewicht van het Item
-        // Is er genoeg ruimte in de Inventory?
-        // Past het Item?
-        // Zet Item in de Dictionary
-        // Return true/false voor succes/mislukt
         return false;
     }
 
     public Item Get(string itemName) {
-        // TODO implementeer:
-        // Zoek Item in de Dictionary
-        // Verwijder Item uit Dictionary (als gevonden)
-        // Return Item of null
         return null;
     }
 
     public int TotalWeight() {
         int total = 0;
-        // TODO implementeer:
-        // Loop door alle items
-        // Tel alle gewichten op
         return total;
     }
 
     public int FreeWeight() {
-        // TODO implementeer:
-        // Vergelijk MaxWeight en TotalWeight()
-        return ...;
+        // return 1;
     }
 }
